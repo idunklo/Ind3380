0efbd2cdbb80322835c62ba0d35b730453e73342
idunklo <idun.klovstad@gmail.com>
2016-01-28 12:44:46 +0100

    making more functions

--- exercise_set1/exercise_12.c
+++ exercise_set1/exercise_12.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void fill1Darray(int n, int *v);
+
 int main()
 {  
     int n;
     n = 10;
     int *v = (int*)malloc(n*sizeof(int));
 
+    fill1Darray(n, v);
+
     for( int i=0; i<n; i++){
-        v[i] = rand();
 
          printf("%d ", v[i]);   
     }
@@ -16,5 +19,21 @@ int main()
     free(v);
 
     return 0;
+
 }
+
+void fill1Darray(int n, int *v)
+{
+    /* Filling an 1D array with random numbers */
+    srand(8);
+    for( int i=0; i<n; i++){
+        v[i] = rand() % 50;
+    }
+}
+
+int find_max()
+{
+    return 0;
+}
+
 
