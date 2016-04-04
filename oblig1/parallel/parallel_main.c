064d744afc7e56388aa36d1118c2de177d6a786b
idunklo <idun.klovstad@gmail.com>
2016-04-04 20:00:11 +0200

    now actually saving before committing

--- oblig1/parallel/parallel_main.c
+++ oblig1/parallel/parallel_main.c
@@ -6,10 +6,85 @@
 
 void import_JPEG_file(const char *filename, unsigned char **image_chars,
         int *image_height, int *image_width,
-        int *num_components);
+        int *num_components);     
+
 void export_JPEG_file(const char *filename, unsigned char *image_chars,
         int image_height, int image_width,
-        int num_components, int quality);
+        int num_components, int quality);  
+
+//making data structure
+typedef struct
+{
+    float* data_storage; /* 1D array to store data */
+    float** image_data; /* a 2D array of floats */  
+    int m;              /* # pixels in y-direction */
+    int n;              /* # pixels in x-direction */
+}
+image; 
+
+//allocate 2D array 
+void allocate_image(image *u, int m, int n)
+{                                            
+    //setting # of pixels in u
+    u -> m = m;
+    u -> n = n;   
+
+    //allocating matrix for image data 
+    u -> data_storage = (float*)malloc(m*n*sizeof(float));
+    u -> image_data =  (float**)malloc(m*sizeof(float*));
+
+    int i;
+    for (i = 0; i < m; i++){
+        u -> image_data[i] = &u -> data_storage[i*n]; } ;
+    
+    return;
+}
+
+//free the storage
+void deallocate_image(image *u)
+{
+    free(u -> data_storage);
+    free(u -> image_data); 
+}
+
+void convert_jpeg_to_image(const unsigned char* image_chars, image *u)
+{
+    int i, j;
+    for (i = 0; i < u -> m; ++i){
+        for (j = 0; j < u -> n; j++){
+            u -> image_data[i][j] = (float)image_chars[i*u->n + j]; 
+        }
+    }
+    return;
+}
+
+void convert_image_to_jpeg(const image *u, unsigned char* image_chars)  
+{
+    int i, j;
+    for (i = 0; i < u -> m; ++i){
+        for (j = 0; j < u -> n; ++j){
+            image_chars[(i*u->n) + j] =(unsigned char) u -> image_data[i][j];
+        }
+    }
+    return;
+}       
+
+void iso_diffusion_denoising(image *u, image *u_bar, float kappa, int iters)
+{ 
+    int counter = 0;   
+    int i, j;
+    while(counter <= iters){
+        for (i = 1; i < u->m -1; i++){  //should it be -2 or -1?
+            for (j = 1;  j <  u -> n -1; j++){ //unsure about the same as above
+                u_bar -> image_data[i][j] = u -> image_data[i][j]
+                    + kappa*(u -> image_data[i-1][j] + u -> image_data[i][j-1] 
+                    - 4*u -> image_data[i][j] + u -> image_data[i][j+1] + u -> image_data[i+1][j]);
+            }
+        }
+    counter++;
+    }
+}     
+
 int main(int argc, char *argv[])
 {
     int m, n, c, iters;
